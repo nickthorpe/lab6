4fd2b3ffc2bb5b9f9fa41bac8bf5f3fca8307571
Nick Thorpe <nick.thorpe@comcast.net>
2013-10-31 22:08:24 -0700

    all required options working

--- js/sign-up.js
+++ js/sign-up.js
@@ -17,14 +17,31 @@ $(function(){
         option.html(state.name);
         stateSelect.append(option);
 	}
+
+	$('.signup-form').submit(function(){
+    	//code to execute when the sign-up form is submitted
+    	//this is the raw DOM form element
+    	//wrap it in a jQuery object so we can use jQuery methods on it
+    	var signupForm = $(this);
+	    //select a descendant input element with the name "addr-1"
+	    var addr1Input = signupForm.find('input[name="addr-1"]');
+	    var addr1Value = addr1Input.val();
+	    var valid = addr1Value.length > 0;
+	    if(valid) {
+	    	var zipInput = signupForm.find('input[name="zip"]');
+    		var zipValue = zipInput.val();
+    		var zipValid = zipValue.length > 0;
+            if (!zipValid){
+            	alert("If you specify an address, you must also list a zip code!");
+            	return false;                        
+			}
+			return valid;	    
+	    }
+	});
                     
 }); //on document ready 
 
-
-$('.signup-form').submit(function(){
-    //code to execute when the sign-up form is submitted
-    //this is the raw DOM form element
-    //wrap it in a jQuery object so we can use jQuery methods on it
-    var signupForm = $(this);
-
-});
+$('.cancel-signup').click(function(){
+    //code to run when user clicks "No Thanks!" button
+    window.location = 'http://www.google.com';
+}); //cancel-signup click
